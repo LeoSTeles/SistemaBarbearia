03007ba34bb9e6d8da167b9904b40cf0c415ba66
Leonardo Teles <leo_steles@hotmail.com>
2023-08-23 09:27:16 -0300

    Criando o model Agendamento e refatorando os models para utilizarem a classe Date

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -10,64 +10,19 @@ import java.util.Date;
  *
  * @author leona
  */
-public class Cliente {
-    private int id;
-    private String nome,email,rg,endereco,cep,telefone;
-    private char sexo;
-    private Date dataNascimento;
+public class Cliente extends Pessoa{
+    private String endereco,cep;
 
-    public Cliente(int id, String nome, String email, String rg, String endereco, String cep, char sexo, Date dataNascimento) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.rg = rg;
+    public Cliente(String endereco, String cep, int id, String nome, String email, String rg, String telefone, char sexo, String dataNascimento) {
+        super(id, nome, email, rg, telefone, sexo, dataNascimento);
         this.endereco = endereco;
         this.cep = cep;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
     }
-
-    public Cliente(int id, String nome, String endereco, String telefone, char sexo) {
-        this.id = id;
-        this.nome = nome;
+    
+    public Cliente(String endereco, String cep, int id, String nome) {
+        super(id, nome);
         this.endereco = endereco;
-        this.telefone = telefone;
-        this.sexo = sexo;
-    }
-
-    
-    
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
+        this.cep = cep;
     }
 
     public String getEndereco() {
@@ -86,29 +41,7 @@ public class Cliente {
         this.cep = cep;
     }
 
-    public char getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
     
-    
+
+   
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -10,60 +10,19 @@ import java.util.Date;
  *
  * @author leona
  */
-public class Usuario {
-    private int id;
-    private String nome,email,rg,telefone,senha,nivelAcesso;
-    private char sexo;
-    private Date dataNascimento;
+abstract public class Usuario extends Pessoa{
+    private String senha,nivelAcesso;
 
-    public Usuario(int id, String nome, String email, String rg, String telefone, String senha, String nivelAcesso, char sexo, Date dataNascimento) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.rg = rg;
-        this.telefone = telefone;
+    public Usuario(String senha, String nivelAcesso, int id, String nome, String email, String rg, String telefone, char sexo, String dataNascimento) {
+        super(id, nome, email, rg, telefone, sexo, dataNascimento);
         this.senha = senha;
         this.nivelAcesso = nivelAcesso;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
     }
 
-    public Usuario(int id, String nome, String senha) {
-        this.id = id;
-        this.nome = nome;
+    public Usuario(int id, String nome,String senha) {
+        super(id, nome);
         this.senha = senha;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
+        this.nivelAcesso = nivelAcesso;
     }
 
     public String getSenha() {
@@ -82,21 +41,6 @@ public class Usuario {
         this.nivelAcesso = nivelAcesso;
     }
 
-    public char getSexo() {
-        return sexo;
-    }
 
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-    
     
 }
